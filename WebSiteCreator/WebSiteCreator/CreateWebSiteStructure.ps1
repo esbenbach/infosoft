11c621041a38892d0c13a9686db7f7bb11c6663d
Esben Bach <esben@ofn.dk>
2016-09-16 06:42:19 +0200

    Changed a few minor things with the script and added support for setting 32bit on the application pools

--- WebSiteCreator/WebSiteCreator/CreateWebSiteStructure.ps1
+++ WebSiteCreator/WebSiteCreator/CreateWebSiteStructure.ps1
@@ -1,20 +1,29 @@
 ﻿#
 # This script requires powershell to be run as Admin in order to work
-#
+# 
+# Before running the script you should have a look at the SiteConfigurations.psd1 file for group sites and configurations as well as setting the default site configuration Web.config
+# 
 # Example Site structure that this script will create:
-#
 #
 # c:\wwwroot\root
 #			\Version    <-- The actual web application that should be created
-#			\SITENAME1
+#			\SITENAME1\
+#					  \Web.config
+#					  \bin    -> ..\Version\bin   - A symlink to the application binary folder 
+#					  \config -> ..\configKey1    - A symlink to configKey1
 #			\SITENAME2
 #			\SITENAME3
-#			\configKey1 <-- Here you place configurations shared by multiple titles but still stuf that should not be global (a symlink is created inside the title directories)
-#			\configKey1
-#			Web.config  <-- The global shared configuration file (the script won't create this for you, its just an indication of the desired design)
+#           \configurations 
+#						   \configKey1 <-- Here you place configurations shared by multiple installations but still stuf that should not be global (a symlink is created inside the title directories)
+#						   \configKey1
+#			Web.config  <-- A global shared configuration file (the script won't create this for you, its just an indication of the desired design)
 # The associated SiteConfigurations.psd1 for various configuration data that drives the script.
 #
-#
+# After the script has completed you need to do the following.
+# 1. Create a config.xml file in each of the "configKey" folders with the references to the web service that the retail web client should use.
+# 2. Copy various logo.jpg to the root of the "SITENAME" folders
+# 3. Copy the default version Web.config to the root folder and remove runtime bindings as well as any serviceModel references that you have overridden in the config.xml file
+# 4. Test that everything works.
 #
 [CmdletBinding()]
 Param(
@@ -82,7 +91,7 @@ $applicationFilePath = "$wwwrootPath\Version";
 # Copy application if path is given, otherwise just create the app filepath
 if($applicationDir -ne "")
 {
-	Copy-Item $applicationDir –destination $applicationFilePath -recurse
+	Copy-Item $applicationDir –destination $applicationFilePath -recurse -Force
 }
 else 
 {
@@ -91,7 +100,7 @@ else
 
 # Create root app pool, and web site
 New-WebAppPool $rootSiteName;
-New-Website -Name $rootSiteName -HostHeader "" -Port $portNumber  -PhysicalPath $wwwrootPath -ApplicationPool $rootSiteName
+New-Website -Name $rootSiteName -HostHeader "" -Port $portNumber -PhysicalPath $wwwrootPath -ApplicationPool $rootSiteName
 
 # Create all applications, directories and links
 foreach($item in $configuration.SiteConfigurationMapping.GetEnumerator())
@@ -102,20 +111,26 @@ foreach($item in $configuration.SiteConfigurationMapping.GetEnumerator())
 	$siteFilePath = "$wwwrootPath\$appName";
 	$applicationName = "$appPrefix$appName";
 	
-	New-Item "$siteFilePath" -ItemType Directory;
-
-	if(!(Test-Path $wwwrootPath\$configName))
+	if (!(Test-Path $siteFilePath))
 	{
-		New-Item "$wwwrootPath\$configName" -ItemType Directory;
+		New-Item "$siteFilePath" -ItemType Directory;
 	}
 
-	New-WebAppPool $siteName;
-
-	New-WebApplication -Name $applicationName -Site $rootSiteName -ApplicationPool $siteName -PhysicalPath $siteFilePath
+	if(!(Test-Path $wwwrootPath\configurations\$configName))
+	{
+		New-Item "$wwwrootPath\configurations\$configName" -ItemType Directory;
+	}
+	
+	if ((Get-WebApplication -Name $siteName) -eq $null)
+	{
+		New-WebAppPool $siteName;
+		Set-ItemProperty "IIS:\apppools\$siteName" -Name $enable32Bit -Value "True"
+		New-WebApplication -Name $applicationName -Site $rootSiteName -ApplicationPool $siteName -PhysicalPath $siteFilePath
+	}
 
 	cmd /c mklink /d "$siteFilePath\bin" "$applicationFilePath\bin";
 	cmd /c mklink "$siteFilePath\Global.asax" "$applicationFilePath\Global.asax";
-	cmd /c mklink /d "$siteFilePath\config" "$wwwrootPath\$configName";
+	cmd /c mklink /d "$siteFilePath\config" "$wwwrootPath\configurations\$configName";
 	
 	$directories = Find-VirtualDirectories $autoDetectVirtualDirectory $applicationFilePath $configuration.VirtualDirectories
 
@@ -126,7 +141,7 @@ foreach($item in $configuration.SiteConfigurationMapping.GetEnumerator())
 
 	# Create default web.config using provided example config, this is not very flexible and should probably be adopted if used for something other than the case it was developed for.
 	$webconfigstring = $configuration.DefaultSiteWebConfig.Replace("{TITLECODEPLACEHOLDER}","$appName");
-	$webconfigstring | Out-File -Encoding utf8 -FilePath "$applicationFilePath\Web.config";
+	$webconfigstring | Out-File -Encoding utf8 -FilePath "$siteFilePath\Web.config";
 }
 
 $watch.Stop();
